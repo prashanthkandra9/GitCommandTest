2954b3718101f38d09bd59464c908e5c59f4c80b
prashanth <prashanthkandra9@gmail.com>
2022-10-30 20:29:54 +0530

    Empty space removed

--- src/test/java/Gitcommands/TestDemo.java
+++ src/test/java/Gitcommands/TestDemo.java
@@ -5,8 +5,6 @@ public class TestDemo {
 	public static void main(String[] args) {
 		System.out.println("Test added from Y1");
 		System.out.println("Test added from Y2");
-
-
 		System.out.println("Xperson added code1");
 		System.out.println("Xperson added code2");
 		System.out.println("Xperson added code3");
